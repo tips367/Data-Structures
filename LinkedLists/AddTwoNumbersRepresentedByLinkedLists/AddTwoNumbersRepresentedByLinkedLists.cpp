e25eed0efa6da18d27ba7c41def5ec71658cd093
khantipu <Tipu.Khan@landisgyr.com>
2021-04-19 23:11:47 +0530

    Adding new program to add two numbers represented by linked lists

---
+++ LinkedLists/AddTwoNumbersRepresentedByLinkedLists/AddTwoNumbersRepresentedByLinkedLists.cpp
@@ -0,0 +1,27 @@
+/* Given two numbers represented by two lists, write a function that returns the sum list. 
+The sum list is list representation of the addition of two input numbers.
+
+Example:
+
+Input:
+List1: 5->6->3 // represents number 365
+List2: 8->4->2 // represents number 248
+Output:
+Resultant list: 3->1->6 // represents number 613
+Explanation: 365 + 248 = 613
+
+Input:
+List1: 7->5->9->4->6 // represents number 64957
+List2: 8->4 // represents number 48
+Output:
+Resultant list: 5->0->0->5->6 // represents number 65005
+Explanation: 64957 + 48 = 65005
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
