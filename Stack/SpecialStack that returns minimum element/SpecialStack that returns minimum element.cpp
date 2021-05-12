84f48185725fad2db7cc4372ad6fd095bbeb9742
khantipu <Tipu.Khan@landisgyr.com>
2021-05-13 00:48:05 +0530

    SpecialStack implementation

--- Stack/SpecialStack that returns minimum element/SpecialStack that returns minimum element.cpp
+++ Stack/SpecialStack that returns minimum element/SpecialStack that returns minimum element.cpp
@@ -24,8 +24,128 @@ When getMin() is called, it should return 18 which is the minimum in the current
 
 #include <iostream>
 
+class Stack 
+{
+
+public:
+    Stack(): m_top(-1){}
+    bool isEmpty();
+    bool isFull();
+    int pop();
+    void push(int x);
+    int top();
+
+private:
+    static const int MAX = 1000;
+    int arr[MAX];
+    int m_top;
+
+};
+
+bool Stack::isEmpty()
+{
+    if (m_top == -1)
+        return true;
+    return false;
+}
+
+bool Stack::isFull()
+{
+    if (m_top == MAX - 1)
+        return true;
+    return false;
+}
+
+int Stack::pop()
+{
+    if (isEmpty()) 
+    {
+        std::cout << "Stack Underflow";
+        return 0;
+    }
+    return arr[m_top--];
+}
+
+void Stack::push(int x)
+{
+    if (isFull()) 
+    {
+        std::cout << "Stack Overflow";
+        return;
+    }
+    arr[++m_top] = x;
+}
+
+int Stack::top()
+{
+    if (isEmpty())
+    {
+        std::cout << "Stack is empty" << std::endl;
+        return 0;
+    }
+    return arr[m_top];
+}
+
+/* A class that supports all the stack operations and one additional operation getMin() that returns the minimum element from stack at
+any time. This class inherits from the stack class and uses an auxiliarry stack that holds minimum elements */
+class SpecialStack : public Stack 
+{
+public:
+    int pop();
+    void push(int x);
+    int getMin();
+
+private:
+    Stack m_min;
+};
+
+void SpecialStack::push(int x)
+{
+    if (isEmpty()) 
+    {
+        Stack::push(x);
+        m_min.push(x);
+    }
+    else 
+    {
+        Stack::push(x);
+        int y = m_min.top();
+        // Update min stack with appropriate minimum values
+        if (x < y)
+            m_min.push(x);
+        else
+            m_min.push(y);
+    }
+}
+
+int SpecialStack::pop()
+{
+    int x = Stack::pop();
+    // Removes top element from min stack also
+    m_min.pop();
+    return x;
+}
+
+int SpecialStack::getMin()
+{
+    return m_min.top();
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    SpecialStack st;
+    st.push(18);
+    st.push(19);
+    st.push(29);
+    std::cout << st.getMin() << std::endl;
+    st.push(15);
+    std::cout << st.getMin() << std::endl;
+    st.push(16);
+    std::cout << st.getMin() << std::endl;
+    st.pop();
+    st.pop();
+    std::cout << st.getMin();
+
+    return 0;
 }
 
