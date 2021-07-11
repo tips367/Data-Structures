047bf491657c5787b40bb98d652a0ac8163c0c65
tips367 <tipu367.khan@hotmail.com>
2021-07-11 19:06:04 +0530

    Find k-th smallest element in BST using : 1. Morris traversal 2. Modified tree data structure

--- Binary Search Tree/Find k-th smallest element in BST/Find k-th smallest element in BST.cpp
+++ Binary Search Tree/Find k-th smallest element in BST/Find k-th smallest element in BST.cpp
@@ -12,6 +12,8 @@ For example, in the following BST, if k = 3, then the output should be 10, and i
 
 #include <iostream>
 
+// Method 1. Using Inorder Traversal.... Time complexity : O(n), Space : O(h)
+/*
 class Node
 {
 public:
@@ -43,7 +45,7 @@ Node* kthSmallest(Node* root, int& k)
 
     // else search in right subtree
     return kthSmallest(root->right, k);
-}
+} 
 
 // A utility function to insert a new node with given key in BST
 Node* insert(Node* root, int value)
@@ -61,6 +63,146 @@ Node* insert(Node* root, int value)
     else
     {
         root->left = insert(root->left, value);
+    }
+
+    // Return root node, after insertion.
+    return root;
+}
+*/
+
+// Method 2. Using Morris Traversal.... Time complexity : O(n), Space : O(1)
+/*
+class Node
+{
+public:
+    int data;
+    Node* left, * right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+Node* kthSmallest(Node* root, int k)
+{
+    if (root == NULL)
+        return NULL;
+
+    Node* kthSmallestNode = NULL;
+
+    Node* current = root;
+    while (current != NULL)
+    {
+        if (current->left == NULL)
+        {
+            k--;
+            if (k == 0)
+                kthSmallestNode = current;
+            current = current->right;
+        }
+        else
+        {
+            // find inorder predecessor of current
+            Node* predecessor = current->left;
+            while (predecessor->right != NULL && predecessor->right != current)
+            {
+                predecessor = predecessor->right;
+            }
+
+            // make current as right child of its inorder predecessor
+            if (predecessor->right == NULL)
+            {
+                predecessor->right = current;
+                current = current->left;
+            }
+            else
+            {
+                // revert the changes, fix the right child of predecessor
+                predecessor->right = NULL;
+                k--;
+                if (k == 0)
+                    kthSmallestNode = current;
+                current = current->right;
+            }
+        }
+    }
+    return kthSmallestNode;
+} 
+
+// A utility function to insert a new node with given key in BST
+Node* insert(Node* root, int value)
+{
+    if (root == NULL)
+    {
+        // Insert the first node, if root is NULL.
+        return new Node(value);
+    }
+
+    if (value > root->data)
+    {
+        root->right = insert(root->right, value);
+    }
+    else
+    {
+        root->left = insert(root->left, value);
+    }
+
+    // Return root node, after insertion.
+    return root;
+}
+*/
+
+// Method 3. Using Modified Tree Data Structure.... Time complexity : O(h), Space : O(h)
+class Node
+{
+public:
+    int data;
+    Node* left, * right;
+    int leftCount;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+        leftCount = 0;
+    }
+};
+
+Node* kthSmallest(Node* root, int k)
+{
+    if (root == NULL)
+        return NULL;
+
+    int count = root->leftCount + 1;
+    if (count == k)
+        return root;
+
+    if (count > k)
+        // search in left subtree
+        return kthSmallest(root->left, k);
+
+    // else search in right subtree
+    return kthSmallest(root->right, k - count);
+}
+
+// A utility function to insert a new node with given key in BST
+Node* insert(Node* root, int value)
+{
+    if (root == NULL)
+    {
+        // Insert the first node, if root is NULL.
+        return new Node(value);
+    }
+
+    if (value > root->data)
+    {
+        root->right = insert(root->right, value);    
+    }
+    else
+    {
+        root->left = insert(root->left, value);
+        // If a node is inserted in left subtree, then leftCount of this node is increased.
+        root->leftCount++;
     }
 
     // Return root node, after insertion.
