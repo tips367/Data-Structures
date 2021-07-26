1067d1c88728e1849ae147440f3c3370d115c521
tips367 <tipu367.khan@hotmail.com>
2021-07-26 23:45:53 +0530

    More methods for Printing a Binary Tree in Vertical Order

--- Hashing/Print a Binary Tree in Vertical Order/Print a Binary Tree in Vertical Order.cpp
+++ Hashing/Print a Binary Tree in Vertical Order/Print a Binary Tree in Vertical Order.cpp
@@ -19,6 +19,11 @@ The output of print this tree vertically will be:
 */
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
 
 class Node
 {
@@ -33,6 +38,7 @@ public:
 };
 
 // Method 1: Time complexity : O(w*n), where w is the width of binary tree having n nodes
+/*
 void findMinMax(Node* node, int& min, int& max, int horizontalDistance)
 {
     if (node == NULL)
@@ -70,6 +76,113 @@ void verticalOrder(Node* root)
     for (int verticalLine = min; verticalLine <= max; verticalLine++)
     {
         printVerticalLine(root, verticalLine, 0);
+        std::cout << "\n";
+    }
+} */
+
+// Method 2: Using hashmap (may not print nodes in same vertical order as they appear in tree)....Time complexity : O(nlogn)
+/*
+void getVerticalOrder(Node* root, std::map<int, std::vector<int>>& m, int horizontalDistance)
+{
+    if (root == NULL)
+        return;
+
+    // Store current node in map 'm'
+    m[horizontalDistance].push_back(root->data);
+
+    // Store nodes in left subtree
+    getVerticalOrder(root->left, m, horizontalDistance - 1);
+
+    // Store nodes in right subtree
+    getVerticalOrder(root->right, m, horizontalDistance + 1);
+}
+
+void verticalOrder(Node* root)
+{
+    std::map<int, std::vector<int>> m;
+    int horizontalDistance = 0;
+    getVerticalOrder(root, m, horizontalDistance);
+
+    // Traverse the map and print nodes at every horizontal distance
+    std::map<int, std::vector<int>> ::iterator it;
+    for (it = m.begin(); it != m.end(); it++)
+    {
+        for (int i = 0; i < it->second.size(); ++i)
+            std::cout << it->second[i] << " ";
+        std::cout << "\n";
+    }
+} */
+
+// Method 3: Using hashmap and set (print nodes in same vertical order as they appear in tree)....Time complexity : O(n * logn * logn * logn)
+/*
+void getVerticalOrder(Node* root, std::map<int, std::map<int, std::multiset<int>>>& m, 
+    int horizontalDistance, int verticalDistance)
+{
+    if (root == NULL)
+        return;
+
+    // Store current node
+    m[horizontalDistance][verticalDistance].insert(root->data);
+
+    // Store nodes in left subtree
+    getVerticalOrder(root->left, m, horizontalDistance - 1, verticalDistance + 1);
+
+    // Store nodes in right subtree
+    getVerticalOrder(root->right, m, horizontalDistance + 1, verticalDistance + 1);
+}
+
+void verticalOrder(Node* root)
+{
+    std::map<int, std::map<int, std::multiset<int>>> m;
+    int horizontalDistance = 0, verticalDistance = 0;
+    getVerticalOrder(root, m, horizontalDistance, verticalDistance);
+
+    // Traverse the map and print nodes
+    for (auto m1: m)
+    {
+        for (auto m2: m1.second)
+        {
+            for(auto m3: m2.second)
+                std::cout << m3 << " ";
+        }
+        std::cout << "\n";
+    }
+} */
+
+// Method 4 : Using Level order traversal....Time complexity : O(n*logn*logn)
+void verticalOrder(Node* root)
+{
+    // Create queue to do level order traversal.
+    // Every item of queue contains node and horizontal distance.
+    std::queue<std::pair<Node*, int>> q;
+    q.push({ root, 0 });
+
+    std::map<int, std::multiset<int>> mapset;
+    while (!q.empty())
+    {
+        int count = q.size();
+
+        // Iterate for all the nodes in the queue currently
+        while (count--)
+        {
+            Node* curr = q.front().first;
+            int horizontalDistance = q.front().second;
+            q.pop();
+            // insert this node's data in map of set
+            mapset[horizontalDistance].insert(curr->data);
+
+            if (curr->left != NULL)
+                q.push({ curr->left, horizontalDistance - 1 });
+            if (curr->right != NULL)
+                q.push({ curr->right, horizontalDistance + 1 });
+        }
+    }
+
+    // Traverse the map and print nodes at every horizontal distance
+    for (auto m1: mapset)
+    {
+        for (auto m2: m1.second)
+            std::cout << m2 << " ";
         std::cout << "\n";
     }
 }
