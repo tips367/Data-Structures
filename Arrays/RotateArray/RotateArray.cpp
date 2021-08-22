97807f2dcecc9f28cfa53b93c4d091f09bcf5917
tips367 <tipu367.khan@hotmail.com>
2021-08-22 21:58:18 +0530

    RotateArray

--- Arrays/RotateArray/RotateArray.cpp
+++ Arrays/RotateArray/RotateArray.cpp
@@ -1,26 +1,43 @@
-// RotateArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Write a function leftRotate(arr[], n, k) that rotates arr[] of size n by k elements to the left.
+
+Example:
+Array : 1,2,3,4,5,6,7
+Rotation of above array by 2 will make array
+3,4,5,6,7,1,2
+
+*/
 
 #include <iostream>
 
-void reverseArray(int arr[], int left, int right)
+// Method 1: Using temp array...Time : O(n), Space : O(k)
+/*
+void leftRotate(int arr[], int n, int k)
 {
-    while (left < right)
-    {
-        int temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
-        left++;
-        right--;
-    }
+    int* temp = new int[k];
+    for (int i = 0; i < k; i++)
+        temp[i] = arr[i];
+
+    for (int i = k; i < n; i++)
+        arr[i - k] = arr[i];
+
+    int j = 0;
+    for (int i = n - k; i < n; i++)
+        arr[i] = temp[j++];
+} */
+
+// Method 2: Rotate one by one...Time : O(n * k), Space : O(1)
+void leftRotateByOne(int arr[], int n)
+{
+    int temp = arr[0];
+    for (int i = 0; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+    arr[n-1] = temp;
 }
 
-void rotateArray(int arr[], int n, int k)
+void leftRotate(int arr[], int n, int k)
 {
-    k = k % n;
-    reverseArray(arr, n - k, n - 1);
-    reverseArray(arr, 0, n-k-1);
-    reverseArray(arr, 0, n-1);
+    for (int i = 0; i < k; i++)
+        leftRotateByOne(arr, n);
 }
 
 void printArray(int A[], int size)
@@ -33,14 +50,14 @@ void printArray(int A[], int size)
 
 int main()
 {
-    int arr[] = {10,20,30};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
     int arr_size = sizeof(arr) / sizeof(arr[0]);
-    int k = 100;
+    int k = 2;
 
     printf("Given array is \n");
     printArray(arr, arr_size);
 
-    rotateArray(arr, arr_size, k);
+    leftRotate(arr, arr_size, k);
 
     printf("\nRotated array is \n");
     printArray(arr, arr_size);
